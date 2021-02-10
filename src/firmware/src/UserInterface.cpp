ef6267f44abe01749de702ea8250e61de89bbf5e
Michael Schnyder <michael.schnyder@outlook.com>
2021-02-10 16:33:55 +0800

    wire oneButton to UI

--- src/firmware/src/UserInterface.cpp
+++ src/firmware/src/UserInterface.cpp
@@ -1,28 +1,60 @@
 #include "UserInterface.h"
-
 #include "pins.h"
-#include <OneButton.h>
 
 OneButton button1 = OneButton(BUTTON1_IN, false, false);
 OneButton button2 = OneButton(BUTTON2_IN, false, false);
 OneButton button3 = OneButton(BUTTON3_IN, false, false);
 
+void raiseButtonEvent(ButtonContext *ctx, Action) {
+  ButtonEvent evt = {ctx->buttonId, Clicked, ctx->button.getPressedTicks()};
+
+  UserInterface* uiCast = (UserInterface *)ctx->ui;
+
+  if (uiCast->buttonActionCallback != NULL) {
+    uiCast->buttonActionCallback(evt);
+  }
+}
+void fClicked(void *s)
+{
+  ButtonContext* ctx = (ButtonContext *)s;
+  Serial.print("Clicked: ");
+  Serial.println(ctx->buttonId);
+  raiseButtonEvent(ctx, Clicked);
+}
+
+void fLongPressStart(void *s)
+{
+  ButtonContext* ctx = (ButtonContext *)s;
+  Serial.print("Long Press Start: ");
+  Serial.println(ctx->buttonId);
+  raiseButtonEvent(ctx, LongPressStart);
+}
+
+void fLongPressEnd(void *s)
+{
+  ButtonContext* ctx = (ButtonContext *)s;
+  Serial.print("Long Press End: ");
+  Serial.println(ctx->buttonId);
+  raiseButtonEvent(ctx, Clicked);
+}
+
 void UserInterface::setup() 
 {
-  Serial.begin(115200);
+  btnCtx1 = (ButtonContext){ 1, button1, this};
+  btnCtx2 = (ButtonContext){ 2, button2, this};
+  btnCtx3 = (ButtonContext){ 3, button3, this};
 
-  button1.attachClick([]() {
-      Serial.println("Clicked Button 1");
-  });
-  button2.attachClick([]() {
-      Serial.println("Clicked Button 2");
-  });
-  button3.attachClick([]() {
-      Serial.println("Clicked Button 3");
-  });
+  button1.attachClick(fClicked, &btnCtx1);
+  button1.attachLongPressStart(fLongPressStart, &btnCtx1);
+  button1.attachLongPressStop(fLongPressEnd, &btnCtx1);    
 
-//   button1.attachLongPressStart(longPressStart);
-//   button1.attachLongPressStop(longPressEnd);    
+  button2.attachClick(fClicked, &btnCtx2);
+  button2.attachLongPressStart(fLongPressStart, &btnCtx2);
+  button2.attachLongPressStop(fLongPressEnd, &btnCtx2);    
+  
+  button3.attachClick(fClicked, &btnCtx3);
+  button3.attachLongPressStart(fLongPressStart, &btnCtx3);
+  button3.attachLongPressStop(fLongPressEnd, &btnCtx3);    
 }
 
 void UserInterface::loop() 
@@ -45,3 +77,7 @@ void UserInterface::loop()
 //   Serial.print(button1.getPressedTicks());
 //   Serial.println(" ms");
 // }
+void UserInterface::raiseEvent(int buttonId, Action action, long duration) 
+{
+    
+}
--- src/firmware/src/UserInterface.h
+++ src/firmware/src/UserInterface.h
@@ -1,11 +1,46 @@
 #ifndef __USERINTERFACE_H__
 #define __USERINTERFACE_H__
+#include <functional>
+
+#include <OneButton.h>
+
+#define BUTTON_PRESS_CALLBACK_TYPE std::function<void(ButtonEvent)>
+
+enum Action {
+    Clicked,
+    LongPressStart,
+    LongPressEnd
+};
+
+struct ButtonEvent
+{
+    int buttonId;
+    Action action;
+    long duration;
+};
+
+struct ButtonContext {
+    int buttonId;
+    OneButton button;
+    void* ui;
+};
 
 class UserInterface {
+
+ButtonContext btnCtx1;
+ButtonContext btnCtx2;
+ButtonContext btnCtx3;
+
+void raiseEvent(int buttonId, Action action, long duration);
 
 public:
     void setup();
     void loop();
+  
+    BUTTON_PRESS_CALLBACK_TYPE buttonActionCallback;
+    void onButtonEvent(BUTTON_PRESS_CALLBACK_TYPE callback) {
+        this->buttonActionCallback = callback;    
+    }
 };
 
 #endif // __USERINTERFACE_H__
