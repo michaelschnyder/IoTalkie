563ca337e705102d1e95906de62a008a3d027301
Michael Schnyder <michael.schnyder@outlook.com>
2021-02-19 10:30:01 +0800

    increase readability of storage stats

--- src/firmware/src/main.cpp
+++ src/firmware/src/main.cpp
@@ -16,21 +16,26 @@ FileUploader uploader;
 
 Application app(&ui, &recorder, &player, &uploader);
 
+String file_size(uint64_t bytes){
+  String fsize = "";
+  if (bytes < 1024)                 fsize = String((long)bytes)+" B";
+  else if(bytes < (1024*1024))      fsize = String(bytes/1024.0,3)+" KB";
+  else if(bytes < (1024*1024*1024)) fsize = String(bytes/1024.0/1024.0,3)+" MB";
+  else                              fsize = String(bytes/1024.0/1024.0/1024.0,3)+" GB";
+  return fsize;
+}
+
 void setup() {
 
   Serial.begin(115200);
   Serial.println();
   
-  SD.begin();
-  Serial.printf("Card total bytes: %i", SD.totalBytes());
-  Serial.println();
-  Serial.printf("Card total used:  %i", SD.usedBytes());
+  SPIFFS.begin();
+  Serial.printf("Storage: %s of %s used", file_size(SPIFFS.usedBytes()).c_str(), file_size(SPIFFS.totalBytes()).c_str());
   Serial.println();
 
-  SPIFFS.begin();
-  Serial.printf("Data total bytes: %i", SPIFFS.totalBytes());
-  Serial.println();
-  Serial.printf("Data total used:  %i", SPIFFS.usedBytes());
+  SD.begin();
+  Serial.printf("SD card:  %s of %s used", file_size(SD.usedBytes()).c_str(), file_size(SD.totalBytes()).c_str());
   Serial.println();
 
   ui.setup();
