ebead61395a1ef04c43f23a88b1d49352000fbb8
Michael Schnyder <michael.schnyder@outlook.com>
2021-02-22 16:16:32 +0800

    moved all scripts to variables

--- src/firmware/lib/SQLite/src/SchemaMigrator.cpp
+++ src/firmware/lib/SQLite/src/SchemaMigrator.cpp
@@ -15,37 +15,37 @@ void SchemaMigrator::runIfMissing(SchemaMigration* migration)
 
         if (!this->hasSchemaVersionInfo()) {
             log_i("Initializing database for schema versioning");
-            this->initializeSchemaInfo();
+            db->execute(SQL_SCHEMA_HISTORY_CREATE);        
         }
-
+        
         currentVersion = getCurrentSchemaVersion();
     }
 
     if (currentVersion < migration->getVersion()) {
-        int nextRank = db->queryInt("SELECT MAX(installed_rank) FROM __schemaHistory;") + 1;
+        long nextRank = db->queryInt(SQL_MIGRATIONS_SELECT_MAX_INSTALLED_RANK) + 1;
         
         log_v("About to insert new entry. Rank: %li, version: %lli", nextRank, migration->getVersion());
 
         // Insert current migration
-        sprintf(buff, "INSERT INTO __schemaHistory (installed_rank, version, description, execution_time, success) VALUES(%li, '%lli', '%s', -1, 0);", 
-                                                    nextRank, migration->getVersion(), migration->getName());
+        sprintf(buff, SQL_NEW_MIGRATION_INSERT, nextRank, migration->getVersion(), migration->getName());
         db->execute(buff);
 
         log_i("Running migration %lli ('%s').", migration->getVersion(), migration->getName());
+
         uint8_t start = millis();
         migration->up(db);
         uint8_t duration = millis() - start;
         
         // Update
         currentVersion = migration->getVersion();
-        sprintf(buff, "UPDATE __schemaHistory SET success = 1, execution_time = %i WHERE installed_rank = %li", duration, nextRank);
+        sprintf(buff, SQL_NEW_MIGRATION_COMPLETE, duration, nextRank);
         db->execute(buff);
     }
 }
 
 uint64_t SchemaMigrator::getCurrentSchemaVersion() 
 {
-    uint64_t dbVersion = db->queryInt("SELECT MAX(version) FROM __schemaHistory WHERE success == 1;");
+    uint64_t dbVersion = db->queryInt(SQL_MIGRATIONS_SELECT_MAX_VERSION);
     log_v("Current version of database is: %lli", dbVersion);
 
     return dbVersion;
@@ -53,20 +53,6 @@ uint64_t SchemaMigrator::getCurrentSchemaVersion()
 
 bool SchemaMigrator::hasSchemaVersionInfo() 
 {
-    int tables = db->queryInt("SELECT count(*) FROM sqlite_master WHERE type ='table' AND name = '__schemaHistory';");
+    int tables = db->queryInt(SQL_SCHEMA_HISTORY_EXISTS);
     return tables != 0;
-}
-
-void SchemaMigrator::initializeSchemaInfo() 
-{
-    const char *create_table =
-    "CREATE TABLE __schemaHistory (\n" 
-    "    \"installed_rank\" INT NOT NULL PRIMARY KEY,\n" 
-    "    \"version\" VARCHAR(50),\n" 
-    "    \"description\" VARCHAR(200) NOT NULL,\n" 
-    "    \"execution_time\" INT NOT NULL,\n" 
-    "    \"success\" BOOLEAN NOT NULL\n" 
-    ");\n";
-
-    db->execute(create_table);
 }
--- src/firmware/lib/SQLite/src/SchemaMigrator.h
+++ src/firmware/lib/SQLite/src/SchemaMigrator.h
@@ -7,12 +7,27 @@
 #define UNKNOWN_VERSION -1
 
 class SchemaMigrator {
-    
+
+    const char* SQL_SCHEMA_HISTORY_EXISTS = "SELECT count(*) FROM sqlite_master WHERE type ='table' AND name = '__schemaHistory';";
+    const char* SQL_MIGRATIONS_SELECT_MAX_VERSION = "SELECT MAX(version) FROM __schemaHistory WHERE success == 1;";
+    const char* SQL_MIGRATIONS_SELECT_MAX_INSTALLED_RANK = "SELECT MAX(installed_rank) FROM __schemaHistory;";
+    const char* SQL_NEW_MIGRATION_INSERT = "INSERT INTO __schemaHistory (installed_rank, version, description, execution_time, success) VALUES(%li, '%lli', '%s', -1, 0);";
+    const char* SQL_NEW_MIGRATION_COMPLETE = "UPDATE __schemaHistory SET success = 1, execution_time = %i WHERE installed_rank = %li";
+    const char* SQL_SCHEMA_HISTORY_CREATE = "CREATE TABLE __schemaHistory (\n" 
+                                            "    \"installed_rank\" INT NOT NULL PRIMARY KEY,\n" 
+                                            "    \"version\" VARCHAR(50),\n" 
+                                            "    \"description\" VARCHAR(200) NOT NULL,\n" 
+                                            "    \"execution_time\" INT NOT NULL,\n" 
+                                            "    \"success\" BOOLEAN NOT NULL\n" 
+                                            ");\n"
+                                            "CREATE UNIQUE INDEX \"idx___schemaHistory_version_success\" ON \"__schemaHistory\" (\"version\" DESC, \"success\" DESC);\n"
+                                            "CREATE UNIQUE INDEX \"idx___schemaHistory_installed_rank\" ON \"__schemaHistory\" (\"installed_rank\" DESC);\n"
+                                            ;
+
     SQLiteDatabase* db;
     uint64_t currentVersion = UNKNOWN_VERSION;
 
     bool hasSchemaVersionInfo();
-    void initializeSchemaInfo();
     uint64_t getCurrentSchemaVersion();
 
 
