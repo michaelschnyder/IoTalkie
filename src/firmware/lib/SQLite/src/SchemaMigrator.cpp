6ae575123ca10a2f430fef99975b50b655f1bc60
Michael Schnyder <michael.schnyder@outlook.com>
2021-02-22 13:00:09 +0800

    run pending migrations

--- src/firmware/lib/SQLite/src/SchemaMigrator.cpp
+++ src/firmware/lib/SQLite/src/SchemaMigrator.cpp
@@ -7,34 +7,46 @@ SchemaMigrator::SchemaMigrator(SQLiteDatabase* db)
 
 void SchemaMigrator::runIfMissing(SchemaMigration* migration) 
 {
-    char buff[24 + 20];
+    log_v("Validating if version %lli '%s' has been applied", migration->getVersion(), migration->getName());
 
-    if (!this->hasSchemaVersionInfo()) {
-        log_i("Initializing database for schema versioning");
-        this->initializeSchemaInfo();
-    }
+    char buff[500];
 
     if (currentVersion == UNKNOWN_VERSION) {
+
+        if (!this->hasSchemaVersionInfo()) {
+            log_i("Initializing database for schema versioning");
+            this->initializeSchemaInfo();
+        }
+
         currentVersion = getCurrentSchemaVersion();
     }
 
     if (currentVersion < migration->getVersion()) {
-        log_i("Running migration %ld.", migration->getVersion());
-
-        migration->up(db);
-        currentVersion = migration->getVersion();
+        int nextRank = db->queryInt("SELECT MAX(installed_rank) FROM __schemaHistory;") + 1;
         
-        sprintf(buff, "PRAGMA user_version = %ld;", currentVersion);        
-        db->queryInt(buff);
+        log_v("About to insert new entry. Rank: %li, version: %lli", nextRank, migration->getVersion());
+
+        // Insert current migration
+        sprintf(buff, "INSERT INTO __schemaHistory (installed_rank, version, description, execution_time, success) VALUES(%li, '%lli', '%s', -1, 0);", 
+                                                    nextRank, migration->getVersion(), migration->getName());
+        db->execute(buff);
+
+        log_i("Running migration %lli ('%s').", migration->getVersion(), migration->getName());
+        uint8_t start = millis();
+        migration->up(db);
+        uint8_t duration = millis() - start;
+        
+        // Update
+        currentVersion = migration->getVersion();
+        sprintf(buff, "UPDATE __schemaHistory SET success = 1, execution_time = %i WHERE installed_rank = %li", duration, nextRank);
+        db->execute(buff);
     }
 }
 
-
 uint64_t SchemaMigrator::getCurrentSchemaVersion() 
 {
-//    long dbVersion = db->queryInt("PRAGMA user_version;");
-    long dbVersion = db->queryInt("PRAGMA table_info(TEST);");
-    log_v("Current version of database is: %ld", dbVersion);
+    uint64_t dbVersion = db->queryInt("SELECT MAX(version) FROM __schemaHistory WHERE success == 1;");
+    log_v("Current version of database is: %lli", dbVersion);
 
     return dbVersion;
 }
