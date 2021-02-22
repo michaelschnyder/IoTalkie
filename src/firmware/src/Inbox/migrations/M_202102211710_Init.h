bfc00cd6cc6711f669509046a212810eb22570ca
Michael Schnyder <michael.schnyder@outlook.com>
2021-02-22 16:16:43 +0800

    init script for mailbox

--- src/firmware/src/Inbox/migrations/M_202102211710_Init.h
+++ src/firmware/src/Inbox/migrations/M_202102211710_Init.h
@@ -5,10 +5,23 @@
 #include <SQLiteDatabase.h>
 
 class M_202102211710_Init : public SchemaMigration {
+
 public:
     M_202102211710_Init() : SchemaMigration(202102211710, "Init") {}
     void up(SQLiteDatabase* db) {
-        delay(123);
+        
+        db->execute("CREATE TABLE \"messages\" (\n"
+                    "	\"messageId\"	VARCHAR(50) NOT NULL UNIQUE,\n"
+                    "	\"timestamp\"	INTEGER     NOT NULL,\n"
+                    "	\"senderId\"	VARCHAR(50) NOT NULL,\n"
+                    "	\"size\"        INTEGER     NOT NULL,\n"
+                    "	\"remoteUrl\"	VARCHAR(255),\n"
+                    "	\"localFile\"	VARCHAR(128),\n"
+                    "	\"playCount\"	INTEGER     NOT NULL DEFAULT 0\n"
+                    ");\n"
+                    );
+
+        db->execute("CREATE UNIQUE INDEX \"idx_messages_messageId\" ON \"inbox\" (\"messageId\" ASC)");
     } 
 };
 #endif // __M_202102211710_INIT_H__
