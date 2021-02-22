1fe8b30a958bacfbbbb7349eb47d6790855b48ac
Michael Schnyder <michael.schnyder@outlook.com>
2021-02-22 12:59:45 +0800

    return correct values

--- src/firmware/lib/SQLite/src/SchemaMigration.h
+++ src/firmware/lib/SQLite/src/SchemaMigration.h
@@ -4,18 +4,19 @@
 
 class SchemaMigration {
 
+private:
     int64_t version;
     const char* name;
 
-    public:
+public:
 
     SchemaMigration(int64_t version, const char* name) {
         this->version = version;
         this->name = name;
     }
 
-    int64_t getVersion() { return version; }
-    const char* getName() { return name ;}
+    int64_t getVersion() { return this->version; }
+    const char* getName() { return this->name ;}
     virtual void up(SQLiteDatabase* db) = 0;
 };
 #endif // __SCHEMAMIGRATION_H__
