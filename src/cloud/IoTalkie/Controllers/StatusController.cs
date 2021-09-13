3d0f678c7e5059edf71de6bb94fce0422d2f4f27
Michael Schnyder <michael.schnyder@outlook.com>
2021-09-13 14:19:15 +0800

    updated controller

---
+++ src/cloud/IoTalkie/Controllers/StatusController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IoTalkie.Controllers
+{
+    [ApiController]
+    [Route("api/status")]
+    public class StatusController : ControllerBase
+    {
+        private readonly ILogger<StatusController> _logger;
+
+        public StatusController(ILogger<StatusController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogDebug("Status endpoint hit");
+            return this.Ok("All fine");
+        }
+    }
+}
