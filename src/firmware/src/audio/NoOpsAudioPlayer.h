4e876d56bad9ab15f58496adf738a38fc7af822a
Michael Schnyder <michael.schnyder@outlook.com>
2021-09-20 19:53:15 +0800

    fix for fix

--- src/firmware/src/audio/NoOpsAudioPlayer.h
+++ src/firmware/src/audio/NoOpsAudioPlayer.h
@@ -1,22 +1,22 @@
 #ifndef __NOOPSAUDIOPLAYER_H__
 #define __NOOPSAUDIOPLAYER_H__
 
+#include <string.h>
 #include "AudioPlayerBase.h"
-
 class NoOpsAudioPlayer : public AudioPlayerBase {
-    private:
-        String currentFilePlaying;
+    
+    char filename[128] = "";
 
     public:
         NoOpsAudioPlayer(int clockPin, int lrcPin, int dataPin) {};
 
         void setup() {};
-        void play(const char* filename) { currentFilePlaying = filename; };
+        void play(const char* filename) { strncpy(this->filename, filename, sizeof(this->filename) - 1); };
         void stop() {};
         void loop() {};
         void setGain(float gain) {};
         bool isPlaying() { return false; };
-        const char* getFilename() { return currentFilePlaying.c_str(); };
+        const char* getFilename() { return filename; };
 
 };
 #endif // __NOOPSAUDIOPLAYER_H__
