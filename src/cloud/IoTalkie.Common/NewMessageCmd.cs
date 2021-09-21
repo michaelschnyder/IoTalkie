b64eee72c14270459764679a7a01cd630601f2b8
Michael Schnyder <michael.schnyder@outlook.com>
2021-09-21 20:28:09 +0800

    move and update all to .NET 5.0

---
+++ src/cloud/IoTalkie.Common/NewMessageCmd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IoTalkie.MessageSenderCmd
+{
+    public class NewMessageCmd
+    {
+        public string Cmd { get; set; } = "newMessage";
+
+        public string MessageId { get; set; } = Guid.NewGuid().ToString();
+
+        public long Timestamp { get; set; } = DateTimeOffset.Now.ToUnixTimeSeconds();
+        
+        public string SenderId { get; set; }
+
+        public long Size { get; set; }
+
+        public string RemoteUrl { get; set; }
+    }
+}
